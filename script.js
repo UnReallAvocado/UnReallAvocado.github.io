2ee4cd28ac6f43df4635598cffc1187ccaa7186e
UnReallAvocado <89562787+UnReallAvocado@users.noreply.github.com>
2024-07-06 16:11:02 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,89 @@
+const body = document.body
+
+const btnTheme = document.querySelector('.fa-moon')
+const btnHamburger = document.querySelector('.fa-bars')
+
+const addThemeClass = (bodyClass, btnClass) => {
+  body.classList.add(bodyClass)
+  btnTheme.classList.add(btnClass)
+}
+
+const getBodyTheme = localStorage.getItem('portfolio-theme')
+const getBtnTheme = localStorage.getItem('portfolio-btn-theme')
+
+addThemeClass(getBodyTheme, getBtnTheme)
+
+const isDark = () => body.classList.contains('dark')
+
+const setTheme = (bodyClass, btnClass) => {
+
+	body.classList.remove(localStorage.getItem('portfolio-theme'))
+	btnTheme.classList.remove(localStorage.getItem('portfolio-btn-theme'))
+
+  addThemeClass(bodyClass, btnClass)
+
+	localStorage.setItem('portfolio-theme', bodyClass)
+	localStorage.setItem('portfolio-btn-theme', btnClass)
+}
+
+const toggleTheme = () =>
+	isDark() ? setTheme('light', 'fa-moon') : setTheme('dark', 'fa-sun')
+
+btnTheme.addEventListener('click', toggleTheme)
+
+const displayList = () => {
+	const navUl = document.querySelector('.nav__list')
+
+	if (btnHamburger.classList.contains('fa-bars')) {
+		btnHamburger.classList.remove('fa-bars')
+		btnHamburger.classList.add('fa-times')
+		navUl.classList.add('display-nav-list')
+	} else {
+		btnHamburger.classList.remove('fa-times')
+		btnHamburger.classList.add('fa-bars')
+		navUl.classList.remove('display-nav-list')
+	}
+}
+
+btnHamburger.addEventListener('click', displayList)
+
+const scrollUp = () => {
+	const btnScrollTop = document.querySelector('.scroll-top')
+
+	if (
+		body.scrollTop > 500 ||
+		document.documentElement.scrollTop > 500
+	) {
+		btnScrollTop.style.display = 'block'
+	} else {
+		btnScrollTop.style.display = 'none'
+	}
+}
+
+document.addEventListener('scroll', scrollUp)
+
+let currentIndex = 0;
+
+let counter = 0; let Title = ['Backend', 'Discord Bot']; let direction = true; let sys = 0
+      aniTitle = setInterval(function () {
+          if (counter == Title[sys].length) direction = false;
+          if (counter == false) direction = true;
+          counter = (direction == true) ? ++counter : --counter;
+
+          newtitle = (counter == 0) ? (sys = (sys === 1 ? 0 : 1), "") : Title[sys].slice(0, counter);
+
+		  document.querySelector('.about__role').textContent = 'A Node.JS and ' + newtitle + ' Developer.';
+      
+      }, 400)
+      
+      window.oncontextmenu = function () {
+        return false;
+      }
+      $(document).keydown(function (event) {
+        if (event.keyCode == 123) {
+          return false;
+        }
+        else if ((event.ctrlKey && event.shiftKey && event.keyCode == 73) || (event.ctrlKey && event.shiftKey && event.keyCode == 74)) {
+          return false;
+        }
+      });
